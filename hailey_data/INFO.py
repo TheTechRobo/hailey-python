31dd04ce590a8080056df684900b2550bc1b27e3
TheTechRobo <52163910+TheTechRobo@users.noreply.github.com>
2021-05-16 11:57:16 -0400

    new configuration option

--- hailey_data/INFO.py
+++ hailey_data/INFO.py
@@ -4,4 +4,5 @@ appeal = None #set this to a message that will be sent in dm's on ban. it will b
 welcomeMessage = ("Hi, welcome to the server! I'm Hailey, the server's moderation bot!"
                    "\nAn older version of Hailey used to exist, and it was more feature-complete. It was written in Java/Kotlin. Unfortunately, API changes rendered the bot unusable without a massive rewrite, which the developer didn't want to do."
                    "\nThis version of Hailey is a rewrite-in-progress in Python by a new developer. We hope you enjoy. Feedback or want to contribute? https://github.com/TheTechRobo/hailey-python")
-leaveMessage = ("Goodbye, it was fun. Have a good time in other servers, hope to see you back soon!")
+leaveMessage = ("Goodbye, it was fun. Have a good time in other servers, hope to see you back soon!") #this might not always send
+DISCORD_MEMBER_INTENT = True #set this to False if you don't want to enable the member intent. doing so will remove the join/leave messages i.e. welcomeMessage and leaveMessage.
